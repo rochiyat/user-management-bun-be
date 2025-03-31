3e01152caf682cb7b75a99fd1ba8af9229566b8e
Rochiyat <rochiyat@gmail.com>
2025-04-01 00:57:49 +0700

    Add role management route to the main router for improved API structure.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userRoute from './user.route';
 import authRoute from './auth.route';
-
+import roleRoute from './role.route';
 const router = Router();
 
 router.use('/users', userRoute);
 router.use('/auth', authRoute);
+router.use('/roles', roleRoute);
 
 export default router;
